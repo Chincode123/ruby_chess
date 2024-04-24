aff4ad6c3b7ebc09a77eb1f6a57fd3a6355700ab
Noah <noah.westerberg2@elev.ga.ntig.se>
2024-04-24 13:57:11 +0200

    queen movement

--- draw.rb
+++ draw.rb
@@ -2,32 +2,33 @@ require "rainbow"
 using Rainbow
 
 def draw_board(board)
+    print " |A|B|C|D|E|F|G|H\n"
     i = 0
     while i < 8
+        print "#{8-i}|"
         j = 0
         while j < 8
             current_space = board[i][j].icon
             if board[i][j].class != Knight
                 current_space += " "
             end
+            
+            if board[i][j].color == nil
+                current_space = Rainbow(current_space).hide
+            elsif board[i][j].color == "w"
+                current_space = Rainbow(current_space).snow
+            else
+                current_space = Rainbow(current_space).black
+            end
 
             if (j + i) % 2 == 0
-                if board[i][j].color == nil
-                    print Rainbow(current_space).bg(:aliceblue).hide
-                elsif board[i][j].color == "w"
-                    print Rainbow(current_space).bg(:aliceblue).white
-                else
-                    print Rainbow(current_space).bg(:aliceblue).brown.bright
-                end
+                current_space = Rainbow(current_space).bg(:bisque)
             else
-                if board[i][j].color == nil
-                    print Rainbow(current_space).bg(:black).hide
-                elsif board[i][j].color == "w"
-                    print Rainbow(current_space).bg(:black).white
-                else
-                    print Rainbow(current_space).bg(:black).brown.bright
-                end
+                current_space = Rainbow(current_space).bg(:black)
             end
+
+            print current_space
+            
             j += 1
         end
         print "\n"
--- pieces.rb
+++ pieces.rb
@@ -21,18 +21,19 @@ end
 #           round_angle(0) => 0
 #           round_angle(1) => 1
 #           round_angle(-1) => -1
-#           round_angle(0.71) => 1
-#           round_angle(0.70) => 0
+#           round_angle(0.70) => 1
+#           round_angle(0.69) => 0
 #           round_angle(0) => 0
 # Datum: 23/4/2024
 # Namn: Noah Westerberg
 def round_angle(num)
-    if num < (Math.sqrt(2) / 2) && num > -(Math.sqrt(2) / 2)
-        return 0
-    elsif num < -(Math.sqrt(2) / 2)
+    # lite marginal för att undvika gränsfall
+    if num < -sqrt(2) + 1.1
         return -1
-    else 
+    elsif num > sqrt(2) - 1.1
         return 1
+    else 
+        return 0
     end
 end
 
@@ -72,6 +73,48 @@ class Piece
         return @position
     end
 
+    # Beskrivning:
+    # Argument 1:
+    # Return: 
+    # Exempel:
+    # Datum: 24/4/2024
+    # Namn: Noah Westerberg
+    def targeted(color)
+        if color = "w"
+            return @targeted_by_white
+        else
+            return @targeted_by_black
+        end
+    end
+
+    # Beskrivning:
+    # Argument 1:
+    # Return: 
+    # Exempel:
+    # Datum: 24/4/2024
+    # Namn: Noah Westerberg
+    def set_targeted(color)
+        if color = "w"
+            @targeted_by_white = true
+        else
+            @targeted_by_black = true
+        end
+    end
+
+    # Beskrivning:
+    # Argument 1:
+    # Return: 
+    # Exempel:
+    # Datum: 24/4/2024
+    # Namn: Noah Westerberg
+    def remove_target(color)
+        if color = "w"
+            @targeted_by_white = false
+        else
+            @targeted_by_black = false
+        end
+    end
+
     # Beskrivning: Muterar pjäsens position. Flytar pjäsen på brädan och lämnar indexet den var på som ett blankt object (inplace). Om det är viktigt för pjäsen så sparas det att den har flytat på sig
     # Argument 1: Cordinets: Positionen som pjäsen ska flyta sig till.
     # Argument 2: 2D-Array: Spelbrädan
@@ -88,11 +131,14 @@ class Piece
             @has_moved = true
         end
     end
+
 end
 
 class Empty < Piece
     def initialize
         @icon = "#"
+        @targeted_by_white = false
+        @targeted_by_black = false
     end
 end
 
@@ -102,6 +148,8 @@ class Pawn < Piece
         @color = color
         @position = position
         @has_moved = false
+        @targeted_by_white = false
+        @targeted_by_black = false
     end
 
     def has_moved
@@ -148,11 +196,14 @@ class King < Piece
         @color = color
         @position = position
         @has_moved = false
+        @targeted_by_white = false
+        @targeted_by_black = false
     end
 
     def has_moved
         return @has_moved
     end
+    
     # Beskrivning: Undersöker vart det är tillgängligt att flytta till.
     # Argument 1: 2D-Array: spelbrädan
     # Return: Cordinets[]: tillgängliga positioner
@@ -164,13 +215,19 @@ class King < Piece
         angle = 0
         while angle < 2 * Math::PI
             new_position = Cordinets.new(@position.x + round_to_one(Math.cos(angle)), @position.y + round_to_one(Math.sin(angle)))
+            angle += Math::PI / 4
+            
             check_square = board[new_position.y][new_position.x]
+            if check_square.targeted
+                next
+            end 
             if check_square.class == Empty
                 positions.append(new_position)
+                board[new_position.y][new_position.x].set_targeted
             elsif check_square.color != @color
                 positions.append(new_position)
+                board[new_position.y][new_position.x].set_targeted
             end
-            angle += Math::PI / 4
         end
 
         return positions
@@ -182,10 +239,42 @@ class Queen < Piece
         @icon = "Q"
         @color = color
         @position = position
+        @targeted_by_white = false
+        @targeted_by_black = false
     end
 
+    # Beskrivning: Undersöker vart det är tillgängligt att flytta till.
+    # Argument 1: 2D-Array: spelbrädan
+    # Return: Cordinets[]: tillgängliga positioner
+    # Exempel:
+    # Datum: 24/4/2024
+    # Namn: Noah Westerberg
     def find_moves(board)
+        positions = []
+        angle = 0
+        while angle < 2 * Math::PI
+            angle += Math::PI / 4
+            
+            i = 0
+            while i < 8
+                new_position = Cordinets.new(@position.x + round_to_one(Math.cos(angle)) + i, @position.y + round_to_one(Math.sin(angle)) + i)
+                i += 1
 
+                check_square = board[new_position.y][new_position.x]
+                if check_square.class == Empty
+                    positions.Add(new_position)
+                    board[new_position.y][new_position.x].set_targeted
+                elsif check_square.color != @color
+                    positions.Add(new_position)
+                    board[new_position.y][new_position.x].set_targeted
+                    break
+                else
+                    break
+                end
+            end
+        end
+
+        return positions
     end
 end
 
@@ -194,6 +283,8 @@ class Knight < Piece
         @icon = "Kn"
         @color = color
         @position = position
+        @targeted_by_white = false
+        @targeted_by_black = false
     end
 
     def find_moves(board)
@@ -207,6 +298,8 @@ class Rook < Piece
         @color = color
         @position = position
         @has_moved = false
+        @targeted_by_white = false
+        @targeted_by_black = false
     end
 
     def has_moved
@@ -223,6 +316,8 @@ class Bishop < Piece
         @icon = "B"
         @color = color
         @position = position
+        @targeted_by_white = false
+        @targeted_by_black = false
     end
 
     def find_moves(board)
