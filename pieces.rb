342394ae228dcc55f0ec0f525b1cb86388c5361a
Noah <noah.westerberg2@elev.ga.ntig.se>
2024-05-02 13:31:14 +0200

    movement: Knight, Rook, Bishop, Queen

--- pieces.rb
+++ pieces.rb
@@ -1,16 +1,15 @@
-
-Cordinets = Struct.new(:x, :y) do
-    def +(cordinets)
-        return Cordinets.new(x + cordinets.x, y + cordinets.y)
+Vector2 = Struct.new(:x, :y) do
+    def +(vector)
+        return Vector2.new(x + vector.x, y + vector.y)
     end
-    def -(cordinets)
-        return Cordinets.new(x - cordinets.x, y - cordinets.y)
+    def -(vector)
+        return Vector2.new(x - vector.x, y - vector.y)
     end
     def *(num)
-        return Cordinets.new(x * num, y * num)
+        return Vector2.new(x * num, y * num)
     end
     def /(num)
-        return Cordinets.new(x / num, y / num)
+        return Vector2.new(x / num, y / num)
     end
 end
 
@@ -32,15 +31,70 @@ def round_angle(num)
         return -1
     elsif num > Math.sqrt(2) - 1.1
         return 1
-    else 
+    else
         return 0
     end
+end
+
+# Beskrivning: Undersöker om en ruta finns på brädan och är en annan färg från pjäsen som använder funktionen och lägger till den i en input array
+# Argument 1: Array, spelbrädan
+# Argument 2: Vector2, positionen av rutan som ska kollas
+# Argument 3: String: användar pjäsens färg
+# Argument 4: Array: output array
+# Return: Inget
+# Exempel:
+# Datum: 30/4/2024
+# Namn: Noah Westerberg
+def check_square(board, square_position, color, out)
+    if square_position.x < board[square_position.y].length && square_position.y < board.length && square_position.x >= 0 && square_position.y >= 0
+        check_square = board[square_position.y][square_position.x]
+        check_square.set_targeted(color)
+        if check_square.class == Empty
+            out.append(square_position)
+            return
+        elsif check_square.color != color
+            out.append(square_position)
+        end
+    end
+end
+
+
+# Beskrivning: Lägger till rutor som finns i samma rad från en startposition och returnerar det.
+# Argument 1: Array, spelbrädan
+# Argument 2: Vector2, användarpjäsens position
+# Argument 3: Vector2, hur marökern som sätter in rutorna justeras
+# Argument 4: String: användar pjäsens färg
+# Return: Array, rutor i en rad
+# Exempel:
+#       Argument3/cursor_shift = (0, 0) => oändlig loop
+# Datum: 29/4/2024
+# Namn: Noah Westerberg
+def check_squares_in_line(board, position, cursor_shift, color)
+    squares = []
+    
+    x = position.x
+    y = position.y
+    while x < board[y].length && y < board.length && x >= 0 && y >= 0
+        check_square = board[y][x]
+        check_square.set_targeted(color)
+        if (check_square.class != Empty)
+            if (check_square.color == color)
+                break
+            end
+        end
+        
+        squares.append(check_square)
+        x += cursor_shift.x
+        y += cursor_shift.y
+    end
+
+    return squares
 end
 
 class Piece
     # Beskrivning: Returnerar pjäsens icon
     # Return: String: pjäsens icon
-    # Exempel: 
+    # Exempel:
     #       Pawn.icon => "P"
     #       Knigth.icon => "Kn"
     #       Piece.icon => error
@@ -60,14 +114,14 @@ class Piece
     def color
         return @color
     end
-    
+
     # Beskrivning: Returnerar pjäsens position/cordinater
-    # Return: Cordinets: pjäsens position
+    # Return: Vector2: pjäsens position
     # Exempel:
-    #       Piece.position => #<struct Cordinets x=0, y=0>
-    #       Piece.position => #<struct Cordinets x=3, y=3>
-    #       Piece.position => #<struct Cordinets x=7, y=5>
-    #       Piece.position => #<struct Cordinets x=nil, y=nil>
+    #       Piece.position => #<struct Vector2 x=0, y=0>
+    #       Piece.position => #<struct Vector2 x=3, y=3>
+    #       Piece.position => #<struct Vector2 x=7, y=5>
+    #       Piece.position => #<struct Vector2 x=nil, y=nil>
     # Datum: 22/4/2024
     # Namn: Noah Westerberg
     def position
@@ -76,7 +130,7 @@ class Piece
 
     # Beskrivning:
     # Argument 1:
-    # Return: 
+    # Return:
     # Exempel:
     # Datum: 24/4/2024
     # Namn: Noah Westerberg
@@ -90,7 +144,7 @@ class Piece
 
     # Beskrivning:
     # Argument 1:
-    # Return: 
+    # Return:
     # Exempel:
     # Datum: 24/4/2024
     # Namn: Noah Westerberg
@@ -104,7 +158,7 @@ class Piece
 
     # Beskrivning:
     # Argument 1:
-    # Return: 
+    # Return:
     # Exempel:
     # Datum: 24/4/2024
     # Namn: Noah Westerberg
@@ -117,7 +171,7 @@ class Piece
     end
 
     # Beskrivning: Muterar pjäsens position. Flytar pjäsen på brädan och lämnar indexet den var på som ett blankt object (inplace). Om det är viktigt för pjäsen så sparas det att den har flytat på sig
-    # Argument 1: Cordinets: Positionen som pjäsen ska flyta sig till.
+    # Argument 1: Vector2: Positionen som pjäsen ska flyta sig till.
     # Argument 2: 2D-Array: Spelbrädan
     # Return: void
     # Exempel:
@@ -165,7 +219,7 @@ class Pawn < Piece
 
     # Beskrivning: Undersöker vart det är tillgängligt att flytta till.
     # Argument 1: 2D-Array: spelbrädan
-    # Return: Cordinets[]: tillgängliga positioner
+    # Return: Vector2[]: tillgängliga positioner
     # Exempel:
     # Datum: 23/4/2024
     # Namn: Noah Westerberg
@@ -214,10 +268,10 @@ class King < Piece
     def has_moved
         return @has_moved
     end
-    
+
     # Beskrivning: Undersöker vart det är tillgängligt att flytta till.
     # Argument 1: 2D-Array: spelbrädan
-    # Return: Cordinets[]: tillgängliga positioner
+    # Return: Vector2[]: tillgängliga positioner
     # Exempel:
     # Datum: 23/4/2024
     # Namn: Noah Westerberg
@@ -225,9 +279,9 @@ class King < Piece
         positions = []
         angle = 0
         while angle < 2 * Math::PI
-            new_position = Cordinets.new(@position.x + round_angle(Math.cos(angle)), @position.y + round_angle(Math.sin(angle)))
+            new_position = Vector2.new(@position.x + round_angle(Math.cos(angle)), @position.y + round_angle(Math.sin(angle)))
             angle += Math::PI / 4
-            
+
             check_square = board[new_position.y][new_position.x]
             check_square.set_targeted(@color)
             if check_square.class == Empty
@@ -256,34 +310,25 @@ class Queen < Piece
 
     # Beskrivning: Undersöker vart det är tillgängligt att flytta till.
     # Argument 1: 2D-Array: spelbrädan
-    # Return: Cordinets[]: tillgängliga positioner
+    # Return: Vector2[]: tillgängliga positioner
     # Exempel:
     # Datum: 24/4/2024
     # Namn: Noah Westerberg
     def find_moves(board)
         positions = []
-        angle = 0
-        while angle < 2 * Math::PI
-            angle += Math::PI / 4
-            
-            i = 0
-            while i < 8
-                new_position = Cordinets.new(@position.x + round_angle(Math.cos(angle)) + i, @position.y + round_angle(Math.sin(angle)) + i)
-                i += 1
-
-                check_square = board[new_position.y][new_position.x]
-                if check_square.class == Empty
-                    positions.Add(new_position)
-                    check_square.set_targeted(@color)
-                elsif check_square.color != @color
-                    positions.Add(new_position)
-                    check_square.set_targeted(@color)
-                    break
-                else
-                    break
+        
+        x_dir = -1
+        while x_dir <= 1
+            y_dir = -1
+            while y_dir <= 1
+                if (x_dir == 0 && y_dir == 0)
+                    y_dir += 1
+                    next
                 end
+                positions.concat(check_squares_in_line(board, @position, Vector2.new(x_dir, y_dir), @color))
+                y_dir += 1
             end
-        end
+            x_dir += 1
 
         return positions
     end
@@ -298,8 +343,27 @@ class Knight < Piece
         @targeted_by_black = false
     end
 
+    # Beskrivning: Undersöker vart det är tillgängligt att flytta till.
+    # Argument 1: 2D-Array: spelbrädan
+    # Return: Vector2 Array: tillgängliga positioner
+    # Exempel:
+    # Datum: 2/5/2024
+    # Namn: Noah Westerberg
     def find_moves(board)
+        positions = []
 
+        x_dir = -1
+        while x_dir <= 1
+            y_dir = -1
+            while y_dir <= 1
+                check_square(board, @position + Vector2.new(3 * x_dir, 1 * y_dir), @color, positions)
+                check_square(board, @position + Vector2.new(1 * x_dir, 3 * y_dir), @color, positions)
+                y_dir += 2
+            end
+            x_dir += 2
+        end
+
+        return positions
     end
 end
 
@@ -317,8 +381,21 @@ class Rook < Piece
         return @has_moved
     end
 
+    # Beskrivning: Undersöker vart det är tillgängligt att flytta till.
+    # Argument 1: 2D-Array: spelbrädan
+    # Return: Vector2 Array: tillgängliga positioner
+    # Exempel:
+    # Datum: 2/5/2024
+    # Namn: Noah Westerberg
     def find_moves(board)
+        positions = []
 
+        positions.concat(check_squares_in_line(board, @position, Vector2.new(1, 0), @color)) 
+        positions.concat(check_squares_in_line(board, @position, Vector2.new(-1, 0), @color)) 
+        positions.concat(check_squares_in_line(board, @position, Vector2.new(0, 1), @color)) 
+        positions.concat(check_squares_in_line(board, @position, Vector2.new(0, -1), @color)) 
+
+        return positions
     end
 end
 
@@ -331,7 +408,25 @@ class Bishop < Piece
         @targeted_by_black = false
     end
 
+    # Beskrivning: Undersöker vart det är tillgängligt att flytta till.
+    # Argument 1: 2D-Array: spelbrädan
+    # Return: Vector2 Array: tillgängliga positioner
+    # Exempel:
+    # Datum: 2/5/2024
+    # Namn: Noah Westerberg
     def find_moves(board)
+        positions = []
 
+        x_dir = -1
+        while x_dir <= 1
+            y_dir = -1
+            while y_dir <= 1
+                positions.concat(check_squares_in_line(board, @position, Vector2.new(x_dir, y_dir), @color))
+                y_dir += 2
+            end
+            x_dir += 2
+        end
+
+        return positions
     end
 end
